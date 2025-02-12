3cbe112e0878aaff155079e5a45e8d132f76c963
Vyacheslav <stormfall228@gmai.com>
2025-02-12 12:37:51 +0500

    Add start script for Railway

--- server/config/db.js
+++ server/config/db.js
@@ -1,23 +1,27 @@
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-    dialect: "postgres",
-    logging: false, // Отключаем лишние логи
-    dialectOptions: process.env.NODE_ENV === "production" ? {
-        ssl: {
-            require: true,
-            rejectUnauthorized: false
-        }
-    } : {}
+const databaseUrl = process.env.NODE_ENV === "production"
+  ? process.env.DATABASE_URL
+  : process.env.DATABASE_URL_LOCAL;
+
+const sequelize = new Sequelize(databaseUrl, {
+  dialect: "postgres",
+  logging: false,
+  dialectOptions: process.env.NODE_ENV === "production" ? {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  } : {}
 });
 
 const ServerDB = async () => {
-    try {
-        await sequelize.authenticate();
-        console.log("✅ Database connected successfully");
-    } catch (error) {
-        console.error("❌ Unable to connect to the database:", error);
-    }
+  try {
+    await sequelize.authenticate();
+    console.log("✅ Database connected successfully");
+  } catch (error) {
+    console.error("❌ Unable to connect to the database:", error);
+  }
 };
 
 ServerDB();
